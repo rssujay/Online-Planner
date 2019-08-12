fa7f8caf575db6dfa49363a013f6476af49a595c
rssujay <>
2019-08-12 16:20:38 +0800

    Add frontend build files to repo

---
+++ frontend/build/precache-manifest.54cb2539e0ac7ec445dfb92b31b83159.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a62a3348751229840cb85c0d2e57cae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "518fcca3230cbc47ac34",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "913be5332fbfc92b55c9",
+    "url": "/static/css/main.dd7ca4c3.chunk.css"
+  },
+  {
+    "revision": "518fcca3230cbc47ac34",
+    "url": "/static/js/2.cc4cb8ca.chunk.js"
+  },
+  {
+    "revision": "913be5332fbfc92b55c9",
+    "url": "/static/js/main.77e17e67.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0a27301dd7c6cefe2d9f976e2b0724df",
+    "url": "/static/media/Icon_faded.0a27301d.svg"
+  }
+]);
